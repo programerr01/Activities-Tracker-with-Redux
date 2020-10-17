888e01a208c2873426a887cbf78d6e2628a7d076
programerr01 <nitesh6pandey6@gmail.com>
2020-10-17 22:48:42 +0530

    Updates

---
+++ precache-manifest.e9f9a73f28cf88d0bff39a0939d8613d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf4a96a7e79a0897a12ec716d41f848",
+    "url": "/Activities-Tracker-with-Redux/index.html"
+  },
+  {
+    "revision": "5ff26f9a8f3d79dadc29",
+    "url": "/Activities-Tracker-with-Redux/static/css/main.329609b1.chunk.css"
+  },
+  {
+    "revision": "1a2cba1c5e872b66c39d",
+    "url": "/Activities-Tracker-with-Redux/static/js/2.afe75461.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Activities-Tracker-with-Redux/static/js/2.afe75461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff26f9a8f3d79dadc29",
+    "url": "/Activities-Tracker-with-Redux/static/js/main.dd1684bf.chunk.js"
+  },
+  {
+    "revision": "59711a4ec5c0a30b9c3d",
+    "url": "/Activities-Tracker-with-Redux/static/js/runtime-main.51b0c096.js"
+  }
+]);
